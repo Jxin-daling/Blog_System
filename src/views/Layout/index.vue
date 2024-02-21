3b790c6d9c9e15bc371b2f47d17b16b135709cd2
Jxin_darling <3167044821@qq.com>
2024-02-21 21:27:50 +0800

    导航栏组件的引入使用

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,14 +1,10 @@
 <template>
-    <router-link to="/home">主页</router-link>
-    <router-link to="/category">分类</router-link>
-    <router-link to="/gallery">画廊</router-link>
-    <router-link to="/message">留言</router-link>
-    <router-link to="/about">关于</router-link>
+    <LayoutNav/>
     <router-view></router-view>
 </template>
 
 <script setup>
-
+import LayoutNav from './components/LayoutNav.vue';
 </script>
 
 <style>
