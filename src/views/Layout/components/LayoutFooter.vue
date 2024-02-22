de21170fc0558084a0351ad6e6523148f5bd1727
Jxin_darling <3167044821@qq.com>
2024-02-22 13:11:46 +0800

    底部栏目的创建

---
+++ src/views/Layout/components/LayoutFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <el-row class="footer">
+          
+        </el-row>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const list = ref([
+    {path:'/',pathname:'主页'},
+    {path:'/category',pathname:'分类'},
+    {path:'/gallery',pathname:'画廊'},
+    {path:'/message',pathname:'留言'},
+    {path:'/about',pathname:'关于'}
+])
+</script>
+
+<style lang="scss" scoped>
+.container{
+    height: 15vh;
+    background: rgba(1, 1, 1, 0.5);
+
+    .footer{
+        
+    }
+}
+</style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,12 @@
 <template>
     <LayoutNav/>
     <router-view></router-view>
+    <LayoutFooter/>
 </template>
 
 <script setup>
 import LayoutNav from './components/LayoutNav.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
 </script>
 
 <style>
