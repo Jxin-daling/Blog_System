e28d9f7717ae86ea3df122c4be140dcaf4d0f38d
Jxin_darling <3167044821@qq.com>
2024-02-24 17:18:17 +0800

    后台组件的基本搭建

--- src/views/Admin/index.vue
+++ src/views/Admin/index.vue
@@ -1,11 +1,30 @@
 <template>
-    admin
+    <div class="container_admin">
+        <el-row>
+            <el-col :span="5">
+                <MenuSlider/>
+            </el-col>
+            <el-col :span="19">
+                <MenuNav/>
+                <AdminMain/>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script setup>
+import MenuSlider from './components/AdminSlider.vue';
+import AdminMain from './components/AdminMain.vue';
+import MenuNav from './components/AdminNav.vue';
+
 
 </script>
 
-<style>
-
+<style lang="scss">
+.container_admin{
+    width: 100vw;
+    height: 100vh;
+    background-color:gray;
+    overflow: hidden;
+}
 </style>
