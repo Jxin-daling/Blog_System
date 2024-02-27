8c0355fca2549ca8b01f704b3d376ef9734e1f55
Jxin_darling <3167044821@qq.com>
2024-02-27 14:41:31 +0800

    刷新状态的监听

--- src/views/Admin/components/AdminMain.vue
+++ src/views/Admin/components/AdminMain.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="container_menu_cotent">
-        <router-view></router-view>
+        <router-view v-if="flag"></router-view>
     </div>
 </template>
 
 <script setup>
+import { ref,watch,nextTick } from "vue";
+import useSettingStore from '@/stores/setting';
+const settingstore = useSettingStore()
+const flag = ref(true)
+
+watch(()=>settingstore.refresh,()=>{
+    flag.value = false
+    nextTick(()=>{
+        flag.value = true
+    })
+})
 </script>
 
 <style>
+
 </style>
