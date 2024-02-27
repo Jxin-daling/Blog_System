c13caaf368b97d50468067fc769d3166c5e392cf
Jxin_darling <3167044821@qq.com>
2024-02-27 22:20:17 +0800

    文章编写基本完成

--- src/views/AricleManage/ArticlePost/index.vue
+++ src/views/AricleManage/ArticlePost/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="container_aclpost">
-    <el-form>
-      <el-form-item>
-        <el-input placeholder="请输入文章标题"/>
+    <el-form
+      :model="aclForm"
+      :rules="rules"
+      ref="formRef"
+    >
+      <el-form-item prop="acltitle">
+        <el-input placeholder="请输入文章标题" v-model="aclForm.acltitle"/>
       </el-form-item>  
     </el-form>
     
@@ -10,23 +14,27 @@
 
       <el-select
         placeholder="请选择分类"
-        v-model="value"
+        v-model="defaultvalue"
         size="large"
+        @change="handlerchange"
       >
         <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
+          v-for="item in categorystore.categorylist.msg"
+          :key="item.cid"
+          :label="item.categoryname"
+          :value="[item.cid,item.categoryname]"
         />
       </el-select>
-
+      当前类别:<el-button>{{ aclForm.category }}</el-button>
     <el-button type="primary" @click="confirm" style="width: 100%;">提交</el-button>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref, reactive,watch } from "vue";
+import { postArticleApi } from '@/apis/article'
+import { useCategoryStore } from '@/stores/category'
+const categorystore = useCategoryStore()
 import {
   Doc,
   Text,
@@ -55,62 +63,62 @@ const extensions = [
   Color
 ];
 
-const value = ref('Option1')
+const defaultvalue = ref('无')
 
-const options = [
-  {
-    value: 'Option1',
-    label: 'Option1',
-  },
-  {
-    value: 'Option2',
-    label: 'Option2',
-  },
-  {
-    value: 'Option3',
-    label: 'Option3',
-  },
-  {
-    value: 'Option4',
-    label: 'Option4',
-  },
-  {
-    value: 'Option5',
-    label: 'Option5',
-  },
-  {
-    value: 'Option6',
-    label: 'Option6',
-  },
-  {
-    value: 'Option7',
-    label: 'Option7',
-  },
-  {
-    value: 'Option8',
-    label: 'Option8',
-  },
-  {
-    value: 'Option9',
-    label: 'Option9',
-  },
-]
-
-// editor's content
+// 内容编写
 const content = ref(`
   <h1>Heading</h1>
   <p>This Editor is awesome!</p>
 `);
 
+
+// 表单数据
+const aclForm = ref({
+    aid:'',
+    acltitle:'',
+    aclcontent:content.value,
+    cid:0,
+    category:''
+})
+
+
+watch(()=>content.value,()=>{
+  // console.log("变了",content.value);
+  aclForm.value.aclcontent = content.value
+})
+
+// 校验规则 
+const rules = reactive({
+  acltitle:[
+        {required:true, message:'标题名称不能为空', trigger:'blur'}
+    ]
+})
+
+const formRef = ref()
+
 // 提交文章
 const confirm = ()=>{
-  console.log("提交喽");
+  console.log(aclForm.value);
+  formRef.value.validate(async(valid)=>{
+      if(valid){
+        postArticleApi(aclForm.value)
+      }
+  })
 }
+
+// 
+const handlerchange = (val)=>{
+  console.log("分类变了",val[0]);
+  console.log("分类变了",val[1]);
+  aclForm.value.cid = val[0]
+  aclForm.value.category = val[1]
+}
+
+onMounted(async()=>{
+  await categorystore.getcategory()
+})
 </script>
 
 <style lang="scss">
-.container_aclpost{
 
-  
-}
 </style>
