8b695ab9059a94a1eadfc0a68fbfbd27fd1375ac
Jxin_darling <3167044821@qq.com>
2024-02-29 11:33:19 +0800

    调整路由等级配置

--- nodeServe/routes/detail.js
+++ nodeServe/routes/detail.js
@@ -4,7 +4,6 @@ const router=express.Router()
 // 获取文章详情信息
 router.get('/getarticledetail',async(req,res)=>{
     const { aid } = req.query
-    console.log(aid);
     let data = await res.execSql(`SELECT aid, title, content, CONCAT('${res.domain}',bannerimgurl) as bannerimgurl, category, DATE_FORMAT(create_time,'%Y-%m-%d %H:%i:%s') as create_time FROM articlestate where aid = ${aid}`)
     res.json({
         status:200,
